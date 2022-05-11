f1841a5d4f3bf5ccef83bff10c449874270f3223
Dirk Schut <dschut@outlook.com>
2022-05-11 23:28:57 +0200

    Some work on skills

---
+++ GPSGame/Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Scriptable/Skill")]
+public class Skill : ScriptableObject{
+    public string ID;
+    public string Name;
+    public string Description;
+    public bool Enabled;
+    public Skill Parent;
+}
